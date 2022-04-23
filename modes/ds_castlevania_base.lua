8401ec8fb245f8c0cd741a5cfadb1471229a2234
Garrett Roberts <chronogeran@gmail.com>
2022-04-23 08:05:25 -0600

    Add ds_castlevania_base, fixes for order_of_ecclesia

---
+++ modes/ds_castlevania_base.lua
@@ -0,0 +1,87 @@
+-- Author: Chronogeran
+-- Common functionality for DS castlevania games to update the map graphics as they are revealed by other players
+
+function addMap(CurrentMapIdAddress, MapExplorationDataAddress, MapPixelDataAddress, MapExplorationDataExtent, spec)
+spec.nextFrameTasks = {}
+spec.tick = function()
+	for k,v in pairs(spec.nextFrameTasks) do
+		v()
+	end
+	spec.nextFrameTasks = {}
+end
+
+local MapPixelDataSize = 0x6000
+local MapTileRowSize = 0x400
+local MapTileSize = 0x20
+
+-- Pixels are stored 4 bits per pixel,
+-- 8x8 pixel tiles
+function getPixelByteIndex(x, y)
+	local tileRowIndex = math.floor(y / 8)
+	local tileColIndex = math.floor(x / 8)
+	return 0x10f040 + tileRowIndex * 0x400 + tileColIndex * 0x20 + (y % 8) * 4 + math.floor((x % 8) / 2)
+end
+
+-- Pixels are stored 4 bits per pixel,
+-- 8x8 pixel tiles
+function pixelDataOffset(x, y)
+	local tileRowIndex = math.floor(y / 8)
+	local tileColIndex = math.floor(x / 8)
+	return MapPixelDataAddress + tileRowIndex * MapTileRowSize + tileColIndex * MapTileSize + (y % 8) * 4 + math.floor((x % 8) / 2)
+end
+
+function pixelCoordinates(pixelOffset)
+	local localOffset = pixelOffset - MapPixelDataAddress
+	local tileX = math.floor((localOffset % MapTileRowSize) / MapTileSize)
+	local pixelX = (tileX * 8) + (localOffset % 4) * 2
+	local tileY = math.floor(localOffset / MapTileRowSize)
+	local pixelY = tileY * 8 + math.floor((localOffset % MapTileSize) / 4)
+	return pixelX, pixelY
+end
+
+spec.custom["mapData"] = function(payload)
+	local mapId, pixelX, pixelY = payload[1], payload[2], payload[3]
+	if mapId ~= memory.readbyte(CurrentMapIdAddress) then return end
+	local i = 4
+	for y=pixelY, pixelY + 4 do
+		for x=pixelX, pixelX + 4, 2 do
+			local offset = pixelDataOffset(x, y)
+			memory.writebyte(offset, payload[i])
+			i = i + 1
+		end
+	end
+end
+
+function sendMapData(localOffset)
+	local pixelX, pixelY = pixelCoordinates(MapPixelDataAddress + localOffset)
+	local payload = {memory.readbyte(CurrentMapIdAddress), pixelX, pixelY}
+	local i = 4
+	for y=pixelY, pixelY + 4 do
+		for x=pixelX, pixelX + 4, 2 do
+			local offset = pixelDataOffset(x, y)
+			payload[i] = memory.readbyte(offset)
+			i = i + 1
+		end
+	end
+	send("mapData", payload)
+end
+
+-- Map exploration
+-- Strategy here: compare graphics data changes on exploration change, then send
+for i=0,MapExplorationDataExtent,2 do
+	spec.sync[MapExplorationDataAddress + i] = {kind="bitOr", size=2, writeTrigger=function(value, previousValue)
+		if value == previousValue then return end
+		local pixelDataBefore = memory.read_bytes_as_array(MapPixelDataAddress, MapPixelDataSize)
+		table.insert(spec.nextFrameTasks, function()
+			local pixelDataAfter = memory.read_bytes_as_array(MapPixelDataAddress, MapPixelDataSize)
+			for i=2,MapPixelDataSize do
+				if pixelDataBefore[i] ~= pixelDataAfter[i] then
+					sendMapData(i - 1) -- account for 1-based array
+					return
+				end
+			end
+		end)
+	end}
+end
+
+end
--- modes/order_of_ecclesia.lua
+++ modes/order_of_ecclesia.lua
@@ -17,153 +17,7 @@ local spec = {
 	custom = {},
 }
 
-spec.nextFrameTasks = {}
-spec.tick = function()
-	for k,v in pairs(spec.nextFrameTasks) do
-		v()
-	end
-	spec.nextFrameTasks = {}
-end
-
-local CurrentMapIdAddress = 0x020ffcb9
-local MapExplorationDataAddress = 0x02100144
-local MapExplorationDataExtent = 0x168
-local MapPixelDataAddress = 0x0214b1e0
-local MapPixelDataSize = 0x6000
-local MapTileRowSize = 0x400
-local MapTileSize = 0x20
-
--- Pixels are stored 4 bits per pixel,
--- 8x8 pixel tiles
-function pixelDataOffset(x, y)
-	local tileRowIndex = math.floor(y / 8)
-	local tileColIndex = math.floor(x / 8)
-	return MapPixelDataAddress + tileRowIndex * MapTileRowSize + tileColIndex * MapTileSize + (y % 8) * 4 + math.floor((x % 8) / 2)
-end
-
-function pixelCoordinates(pixelOffset)
-	local localOffset = pixelOffset - MapPixelDataAddress
-	local tileX = math.floor((localOffset % MapTileRowSize) / MapTileSize)
-	local pixelX = (tileX * 8) + (localOffset % 4) * 2
-	local tileY = math.floor(localOffset / MapTileRowSize)
-	local pixelY = tileY * 8 + math.floor((localOffset % MapTileSize) / 4)
-	return pixelX, pixelY
-end
-
-spec.custom["mapData"] = function(payload)
-	local mapId, pixelX, pixelY = payload[1], payload[2], payload[3]
-	if mapId ~= memory.readbyte(CurrentMapIdAddress) then return end
-	local i = 4
-	for y=pixelY, pixelY + 4 do
-		for x=pixelX, pixelX + 4, 2 do
-			local offset = pixelDataOffset(x, y)
-			memory.writebyte(offset, payload[i])
-			i = i + 1
-		end
-	end
-end
-
-function sendMapData(localOffset)
-	local pixelX, pixelY = pixelCoordinates(MapPixelDataAddress + localOffset)
-	local payload = {memory.readbyte(CurrentMapIdAddress), pixelX, pixelY}
-	local i = 4
-	for y=pixelY, pixelY + 4 do
-		for x=pixelX, pixelX + 4, 2 do
-			local offset = pixelDataOffset(x, y)
-			payload[i] = memory.readbyte(offset)
-			i = i + 1
-		end
-	end
-	send("mapData", payload)
-end
-
--- Map exploration
-for i=0,MapExplorationDataExtent do
-	spec.sync[MapExplorationDataAddress + i] = {kind="bitOr", writeTrigger=function(value, previousValue)
-		if value == previousValue then return end
-		local pixelDataBefore = memory.read_bytes_as_array(MapPixelDataAddress, MapPixelDataSize)
-		table.insert(spec.nextFrameTasks, function()
-			local pixelDataAfter = memory.read_bytes_as_array(MapPixelDataAddress, MapPixelDataSize)
-			for i=2,MapPixelDataSize do
-				if pixelDataBefore[i] ~= pixelDataAfter[i] then
-					sendMapData(i - 1) -- account for 1-based array
-					return
-				end
-			end
-		end)
-	end}
-end
-
--- TODO alignment
--- Glyphs
-for i=0,0x31 do
-	spec.sync[0x021003ea + i] = {size=1, mask=0xf, kind="delta", receiveTrigger=function (value, previousValue)
-		-- Get guide data
-		-- TODO test
-		if previousValue == 0 then
-			local val = memory.readbyte(0x021003ea + i)
-			memory.writebyte(0x021003ea + i, OR(0x90, val))
-		end
-	end}
-end
--- R Glyphs
-for i=0,0x16 do
-	spec.sync[0x02100421 + i] = {size=1, mask=0xf, kind="delta", receiveTrigger=function (value, previousValue)
-		-- Get guide data
-		-- TODO test
-		if previousValue == 0 then
-			local val = memory.readbyte(0x02100421 + i)
-			memory.writebyte(0x02100421 + i, OR(0x90, val))
-		end
-	end}
-end
-
--- Relics
-for i=0,5 do
-spec.sync[0x02100458 + i] = {size=1, mask=0xf,
-	receiveTrigger=function (value, previousValue)
-		-- Equip abilities when receiving them
-		if value == 1 then
-			memory.writebyte(0x02100458 + i, 0x91)
-		end
-	end
-}
-end
-
--- Bestiary: enemies defeated
-for i=0,0xf do
-	spec.sync[0x02100344 + i] = {kind="bitOr"}
-end
--- Bestiary: Item 1 drop
-for i=0,0xf do
-	spec.sync[0x02100354 + i] = {kind="bitOr"}
-end
--- Bestiary: Item 2 drop
-for i=0,0xf do
-	spec.sync[0x02100364 + i] = {kind="bitOr"}
-end
-
--- TODO verify space/alignment
--- Area visit flags (for area name popup)
-spec.sync[0x02100378] = {size=4, kind="bitOr"}
--- TODO verify event space/alignment
--- Events, item pickups (todo: don't know end range)
-for i=0,0x7f do
-	spec.sync[0x02100378 + i] = {kind="bitOr"}
-end
-
--- TODO alignment
--- Item Inventory
-for i=0,0xea do
-	spec.sync[0x0210045e + i] = {size=1, mask=0xf, kind="delta", receiveTrigger=function (value, previousValue)
-		-- Get guide data
-		-- TODO test
-		if previousValue == 0 then
-			local val = memory.readbyte(0x0210045e + i)
-			memory.writebyte(0x0210045e + i, OR(0x90, val))
-		end
-	end}
-end
+-- TODO auto equip magnes
 
 -- HP
 --spec.sync[0x021002b4] = {size=2, kind="delta"}
@@ -179,10 +33,66 @@ end
 --spec.sync[0x021002be] = {size=2}
 
 -- EXP
-spec.sync[0x0210030c] = {size=4, kind="delta", deltaMin=0}
+spec.sync[0x0210030c] = {size=4, kind="delta", deltaMin=0, deltaMax=99999999}
 -- Gold
-spec.sync[0x02100310] = {size=4, kind="delta", deltaMin=0}
+spec.sync[0x02100310] = {size=4, kind="delta", deltaMin=0, deltaMax=9999999}
+
+local CurrentMapIdAddress = 0x020ffcb9
+local MapExplorationDataAddress = 0x02100144
+local MapPixelDataAddress = 0x0214b1e0
+local MapExplorationDataExtent = 0x168
+require("modes.ds_castlevania_base")
+addMap(CurrentMapIdAddress, MapExplorationDataAddress, MapPixelDataAddress, MapExplorationDataExtent, spec)
+
+-- Bestiary: enemies defeated
+for i=0,0xf,4 do
+	spec.sync[0x02100344 + i] = {size=4, kind="bitOr"}
+end
+-- Bestiary: Item 1 drop
+for i=0,0xf,4 do
+	spec.sync[0x02100354 + i] = {size=4, kind="bitOr"}
+end
+-- Bestiary: Item 2 drop
+for i=0,0xf,4 do
+	spec.sync[0x02100364 + i] = {size=4, kind="bitOr"}
+end
+
+-- RC
+-- Flags (area visit flags, events, item pickups, area unlocks, boss fights)
+for i=0,0x6f,4 do
+	spec.sync[0x02100378 + i] = {size=4, kind="bitOr"}
+end
+-- 3d8 may not be something I need?
+
+-- Glyphs
+for i=0,0x31,4 do
+	spec.sync[0x021003e8 + i] = {size=4, kind="bitOr"}
+end
+-- R Glyphs
+for i=0,0x17,4 do
+	spec.sync[0x02100420 + i] = {size=4, kind="bitOr"}
+end
+
+-- Relics & Inventory
+for i=0,0xf0,4 do
+	spec.sync[0x02100458 + i] = {size=4, mask=0x0f0f0f0f, kind="delta", receiveTrigger=function (value, previousValue)
+		-- Get guide data
+		-- TODO test
+		local changedBits = XOR(value, previousValue)
+		if changedBits == 0 then return end
+		local bitsToOr = 0x90
+		if i <= 4 then bitsToOr = 0xd0 end -- Equip relics with bit 0x40
+		local guideData = 0
+		for j=0,3 do
+			if AND(changedBits, SHIFT(0xf, -8 * j)) ~= 0 then
+				guideData = OR(guideData, SHIFT(bitsToOr, -8 * j))
+			end
+		end
+		memory.writedword(0x02100458 + i, OR(guideData, memory.readdword(0x02100458 + i)))
+	end}
+end
+
 -- Kills
-spec.sync[0x021005e8] = {size=4, kind="delta", deltaMin=0}
+spec.sync[0x021005e8] = {size=4, kind="delta", deltaMin=0, deltaMax=99999}
 
 return spec
